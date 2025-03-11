3fd849743ea2c65e2eea05c66baebd7b043058d4
Karen-Rosas-174537 <k174537@unicamp.br>
2025-03-11 14:58:23 -0300

    class robo

---
+++ Robo.java
@@ -0,0 +1,15 @@
+class Robo {
+    private String nome;    //nome do robô
+    private int posicaoX;   //coordenada X no Ambiente
+    private int posicaoY;   //coordenada Y no Ambiente
+
+
+//    Construtor para inicializar os atributos;
+// mover(int deltaX, int deltaY) - Atualiza a posi ̧c ̃ao do robˆo;
+//• exibirPosicao() - Imprime a posi ̧c ̃ao atual do robˆo.
+
+
+
+
+
+}
