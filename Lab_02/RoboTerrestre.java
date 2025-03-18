f849d5ba1e413d29579ebe451f31ea2c3f8d1ffd
caio lima albuquerque <c288808@dac.unicamp.br>
2025-03-18 15:11:33 -0300

    comentarios na classe robo terrestre

--- Lab_02/RoboTerrestre.java
+++ Lab_02/RoboTerrestre.java
@@ -3,7 +3,7 @@ public class RoboTerrestre extends Robo{
     private int tempoLocomocaoTerrestre; //Tempo que o robô demora em uma locomoção
     
     public RoboTerrestre(String nome, String direcao, int posicaoX, int posicaoY, int velocidadeMaxima){
-        super(nome, direcao, posicaoX, posicaoY);
+        super(nome, direcao, posicaoX, posicaoY); //Herança da classe robô
         this.velocidadeMaxima = velocidadeMaxima;
         this.tempoLocomocaoTerrestre = 7;
     }
@@ -11,9 +11,9 @@ public class RoboTerrestre extends Robo{
     public boolean mover(int deltaX){
         if (Math.sqrt(deltaX*deltaX) < velocidadeMaxima){
             super.mover(deltaX, 0); //Como o robô é terrestre não faz sentido ele se mover na vertical
-            return true; 
+            return true; //Retorna se o robo puder se mover
         }else{
-            return false;
+            return false; //Retorna se o robo iria ultrapassar a velocidade máxima
             // System.out.println("O " + getNome() + " quer ir rápido demais! tente desacelerar um pouco");
         }
     }
