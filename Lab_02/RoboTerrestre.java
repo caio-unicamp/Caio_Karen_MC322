c1d961974ec3c02c5be96023214c375182eab651
caio lima albuquerque <c288808@dac.unicamp.br>
2025-03-18 15:06:25 -0300

    algumas alterações na classe do robo terrestre

--- Lab_02/RoboTerrestre.java
+++ Lab_02/RoboTerrestre.java
@@ -1,20 +1,18 @@
 public class RoboTerrestre extends Robo{
-    private int velocidadeMaxima;
-    private int tempoLocomocao;
+    private int velocidadeMaxima; //Velocidade máxima permitida para o robô
+    private int tempoLocomocaoTerrestre; //Tempo que o robô demora em uma locomoção
     
     public RoboTerrestre(String nome, String direcao, int posicaoX, int posicaoY, int velocidadeMaxima){
         super(nome, direcao, posicaoX, posicaoY);
         this.velocidadeMaxima = velocidadeMaxima;
-        this.tempoLocomocao = 7;
+        this.tempoLocomocaoTerrestre = 7;
     }
 
-    public void mover(int deltaX, int deltaY){
-        if (Math.sqrt(deltaY*deltaY + deltaX*deltaX) < velocidadeMaxima){
-            super.mover(deltaX, deltaY);
+    public void mover(int deltaX){
+        if (Math.sqrt(deltaX*deltaX) < velocidadeMaxima){
+            super.mover(deltaX, 0); //Como o robô é terrestre não faz sentido ele se mover na vertical
         }else{
             System.out.println("O " + getNome() + " quer ir rápido demais! tente desacelerar um pouco");
         }
     }
-
-
 } 
