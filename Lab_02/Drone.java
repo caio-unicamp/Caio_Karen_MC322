e646946a3823d5ba27e286032270e910cc2f39d8
caio lima albuquerque <c288808@dac.unicamp.br>
2025-03-18 17:15:00 -0300

    Cria classes herdeiras de Robo Aereo

---
+++ Lab_02/Drone.java
@@ -0,0 +1,6 @@
+public class Drone extends RoboAereo{
+
+    public Drone(String nome, String direcao, int x, int y, int altitude){
+        super(nome, direcao, x, y, altitude);
+    }
+}
---
+++ Lab_02/Passaro.java
@@ -0,0 +1,6 @@
+public class Passaro extends RoboAereo{
+
+    public Passaro(String nome, String direcao, int x, int y, int altitude){
+        super(nome, direcao, x, y, altitude);
+    }
+}
