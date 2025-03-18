f1e32fa04f559008c79142487c7ad0729511b3c7
caio lima albuquerque <c288808@dac.unicamp.br>
2025-03-18 16:04:47 -0300

    Cria a lista de robos ativos e começa a implementar a função de adicionar robos nessa lista

--- Lab_02/Ambiente.java
+++ Lab_02/Ambiente.java
@@ -1,12 +1,16 @@
+import java.util.ArrayList;
+
 public class Ambiente{
     private String ambiente; //nome do ambiente
     private int largura; //largura do ambiente
     private int altura; //altura do ambiente
+    private ArrayList<Robo> listaRobosAtivos;
 
     public Ambiente(String ambiente, int largura, int altura){ //Construtor para inicializar os atributos
         this.ambiente = ambiente;
         this.largura = largura;
         this.altura = altura;
+        listaRobosAtivos = new ArrayList<>();
     }
 
     public String getNomeAmbiente(){ //retorna o nome do ambiente 
@@ -20,5 +24,9 @@ public class Ambiente{
 
     public boolean dentroDosLimites(int x, int y){ //Retorna um booleano para analisar se o robô está dentro dos limites
         return (x >= 0 && x <= this.largura && y >= 0 && y <= this.altura);
+    }
+
+    public void adicionarRobo(){
+
     }
 }
