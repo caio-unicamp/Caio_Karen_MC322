147b3507e4b48d4bc7999c04136adc0fea35f0da
caio lima albuquerque <c288808@dac.unicamp.br>
2025-03-18 16:36:41 -0300

    modificações na análise se o robô está dentro dos limites para considerar altura para robos aereos

--- Lab_02/Ambiente.java
+++ Lab_02/Ambiente.java
@@ -2,8 +2,9 @@ import java.util.ArrayList;
 
 public class Ambiente{
     private String ambiente; //nome do ambiente
-    private int tamX; //tamX do ambiente
-    private int tamY; //tamY do ambiente
+    private int tamX; //tamanho em X do ambiente
+    private int tamY; //tamanho em Y do ambiente
+    private int altura; //Altura máxima do ambiente
     private ArrayList<Robo> listaRobosAtivos;
 
     public Ambiente(String ambiente, int tamX, int tamY){ //Construtor para inicializar os atributos
@@ -22,8 +23,8 @@ public class Ambiente{
         return limites;
     }
 
-    public boolean dentroDosLimites(int x, int y){ //Retorna um booleano para analisar se o robô está dentro dos limites
-        return (x >= 0 && x <= this.tamX && y >= 0 && y <= this.tamY);
+    public boolean dentroDosLimites(int x, int y, int altura){ //Retorna um booleano para analisar se o robô está dentro dos limites
+        return (x >= 0 && x <= this.tamX && y >= 0 && y <= this.tamY && altura >= 0 && altura <= this.altura);
     }
 
     public void adicionarRobo(Robo robo){ //Adiciona os robos ativos na lista 
