b07387a7d266337ef032f2b5e61d484863d2bc91
caio lima albuquerque <c288808@dac.unicamp.br>
2025-03-25 18:19:47 -0300

    Concerta as interações pros robos aéreros

--- Lab_02/Main.java
+++ Lab_02/Main.java
@@ -5,17 +5,20 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int comando = 1;
 
-        System.out.println("No começo de tudo, crie seu ambiente!\nNome: \n Largura: \nAltura: \nProfundidade: ");
+        System.out.println("No começo de tudo, crie seu ambiente!\nNome: ");
         String nomeAmbiente = scanner.nextLine();
+        System.out.println("Largura: \n");
         int x = scanner.nextInt();
+        System.out.println("Altura: \n");
         int z = scanner.nextInt();
+        System.out.println("Profundidade: \n");
         int y = scanner.nextInt();
         Ambiente ambiente = new Ambiente(nomeAmbiente, x, y, z); //Cria seu novo ambiente
         while (comando != 0){ //Cria um looping para as ações possíveis
             System.out.print("\033[H\033[2J"); //Da clear no terminal depois de cada interação
             System.out.flush();
 
-            System.out.print("Digite um comando: \n0 - Encerrar\n1 - Criar um Robô\n2 - Controlar um Robô\n3 - Verificar lista de Robôs");
+            System.out.print("Digite um comando: \n0 - Encerrar\n1 - Criar um Robô\n2 - Controlar um Robô\n3 - Verificar lista de Robôs\n");
             comando = scanner.nextInt();
             if (comando == 0){
                 break;
@@ -26,19 +29,27 @@ public class Main {
                     System.out.println("Bleh, odeio essas pestes infernizando nossos áres... tá, como você quer que ele seja?\n1 - Drone\n2 - Pássaro\n");
                     comando = scanner.nextInt();
                     if (comando == 1){
-                        System.out.println("É... pelo menos esse daí é útil para algo, tá, agora só preciso saber as informações finais do seu Robô\nNome: \nDireção: \nPosição X: \nPosição Y: \nPosição Z: \n");    
+                        System.out.println("É... pelo menos esse daí é útil para algo, tá, agora só preciso saber as informações finais do seu Robô\nNome: \n");    
                         String nomeDrone = scanner.nextLine();
+                        System.out.println("Direção: \n");
                         String direcaoDrone = scanner.nextLine();
+                        System.out.println("Posição X: \n");
                         int posicaoXdrone = scanner.nextInt();
+                        System.out.println("Posição Y: \n");
                         int posicaoYdrone = scanner.nextInt();
+                        System.out.println("Posição Z: \n");
                         int posicaoZdrone = scanner.nextInt();
                         Drone drone = new Drone(nomeDrone, direcaoDrone, posicaoXdrone, posicaoYdrone, posicaoZdrone);
                     }else if (comando == 2){
-                        System.out.println("Eu desisto, faz o que você quiser aí...\nNome: \nDireção: \nPosição X: \nPosição Y: \nPosição Z: \n");
+                        System.out.println("Eu desisto, faz o que você quiser aí...\nNome: \n");
                         String nomePassaro = scanner.nextLine();
+                        System.out.println("Direção: \n");
                         String direcaoPassaro = scanner.nextLine();
+                        System.out.println("Posição X: \n");
                         int posicaoXpassaro = scanner.nextInt();
+                        System.out.println("Posição Y: \n");
                         int posicaoYpassaro = scanner.nextInt();
+                        System.out.println("Posição Z: \n");
                         int posicaoZpassaro = scanner.nextInt();
                         Passaro passaro = new Passaro(nomePassaro, direcaoPassaro, posicaoXpassaro, posicaoYpassaro, posicaoZpassaro);
                     }
@@ -64,7 +75,11 @@ public class Main {
                     }
                 }
             }else if (comando == 2){
-
+                System.out.println("Vamos ver do que esses pequeninos são capazes, mas antes escolha com qual deles você quer se divertir agora\n");
+                int contadorRobo = 1;
+                for (Robo robo : ambiente.getLista()){
+                    System.out.println(contadorRobo + " - " + robo.getNome());
+                }
             }else if (comando == 3){
 
             }
