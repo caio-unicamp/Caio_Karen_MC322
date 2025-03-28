ce80e9f30aa116f517fe8c7b94eab580fc9f4bce
caio lima albuquerque <c288808@dac.unicamp.br>
2025-03-28 11:00:25 -0300

    Conserta a função eliminarRobo e adiciona uma função para retornar o número de robôs eliminados

--- Lab_02/Aspirador.java
+++ Lab_02/Aspirador.java
@@ -4,32 +4,27 @@ import java.util.ArrayList;
 public class Aspirador extends RoboTerrestre{
     //atributo numero de robos que eliminou
     private int robosEliminados = 0;
+    private Ambiente ambiente;
    
     //Construtor para inicializar os atributos
     public Aspirador(String nome, String direcao, int x, int y, int velocidadeMaxima, Ambiente ambiente){
         super(nome, direcao, x, y, velocidadeMaxima, ambiente);
         this.robosEliminados = 0;   //inicializar o atritubo próprio
+        this.ambiente = ambiente;
     }
 
     //método de eliminar
-    public void eliminar(String nome) {
+    public boolean eliminarRobo(){ //Função para eliminar robôs quando ele se mover
         //pegar a lista de robos e percorrer procurando o nome do robo
         for (Robo robo : ambiente.getLista()) {
-            private ArrayList<Robo> listaRobosEliminar;
-        } 
-        listaRobosEliminar = ambiente.getLista();   //pegar a lista de robos do ambiente
-        //percorrer a lista
-        for (int i = 0; i < ambiente.getLista().size(); i++) {
-            //se a posição do robo na índice i for igual a posição do robo que queremos eliminar
-            if ((Robo.listaRobosAtivos.get(i).posicaoX.equals(x)) && (Robo.listaRobosAtivos.get(i).posicaoY.equals(y))){
-                //remover o robo da lista
-                Robo.listaRobosAtivos.remove(i);
-                return true; // Removido com sucesso
-                this.robosEliminados++; //incrementar o atributo próprio
+            if (this.getPosicao()[0] == robo.getPosicao()[0] && this.getPosicao()[1] == robo.getPosicao()[1] ){ //Se ele achou um robô na mesma posição que ele está, ele elimina esse robô 
+                ambiente.getLista().remove(robo); //Remove o robô encontrado da lista de robôs ativos
+                robosEliminados++; 
+                return true; //Se ele eliminou o robô ele retorna true
             }
-        }
+        } 
+        return false; //Se ele percorreu a lista inteira e não existe robô com o nome dado ele retorna falso
 
-        return false; //robo não encontrado se ele não retornou true previamente
     }
     
     public int getRobosEliminados(){ //Função que retorna o número de robôs que foram eliminados
@@ -37,7 +32,7 @@ public class Aspirador extends RoboTerrestre{
     }
 
     //método de se mover
-    public void mover(int deltaX, int deltaY){             
+    public void mover(int deltaX, int deltaY){ //A classe aspirador              
         //se encontrar um obstáculo
         if (super.identificarRobo(this.x + deltaX, this.y + deltaY)){
             //eliminar o robô
