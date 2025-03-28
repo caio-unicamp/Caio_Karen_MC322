d81fae67649dff525819b34b41412b4a882c49bb
caio lima albuquerque <c288808@dac.unicamp.br>
2025-03-28 12:25:32 -0300

    Conserta o constructor do Drone para conseguir receber o tempo de locomoção do rover e melhora a função entregar pacote com um return false que estava redundante

--- Lab_02/Drone.java
+++ Lab_02/Drone.java
@@ -2,20 +2,21 @@ public class Drone extends RoboAereo{
     //atributo próprio é um robô que será entregue pelo drone
     Robo pacote;
     Ambiente ambiente;
-    int posicaoXdrone, posicaoYdrone, posicaoZdrone;
+    int posicaoXdrone, posicaoYdrone, posicaoZdrone, tempoLocomocaoPacote;
     //Construtor para inicializar os atributos
-    public Drone(String nome, String direcao, int x, int y, int altitude, Ambiente ambiente){
+    public Drone(String nome, String direcao, int x, int y, int altitude, Ambiente ambiente, int tempoLocomocaoTerrestre){
         //inicializar os atributos do drone
         super(nome, direcao, x, y, altitude, ambiente);
         this.ambiente = ambiente;
         this.posicaoXdrone = x;
         this.posicaoYdrone = y;
         this.posicaoZdrone = altitude;
+        this.tempoLocomocaoPacote = tempoLocomocaoTerrestre;
     }
     //método de se mover
     public boolean entregarPacote(int posicaoXdronefinal, int posicaoYdronefinal){ //Função para entregar um pacote. Essa função é um booleano pois caso o pacote seja entregue retorna true e caso não seja, retorna false
         //inicializar o robo pacote
-        pacote = new Rover("Pacote1", "Sul", this.posicaoXdrone, this.posicaoYdrone, this.posicaoZdrone, ambiente);
+        pacote = new Rover("Pacote1", "Sul", this.posicaoXdrone, this.posicaoYdrone, this.posicaoZdrone, ambiente, tempoLocomocaoPacote);
         ambiente.adicionarRobo(pacote); //Adiciona o pacote na lista de Robôs ativos
         mover(posicaoXdronefinal - posicaoXdrone, posicaoYdronefinal - posicaoYdrone); //Move o drone até a posição final da entrega e na função mover ele identifica se encontra outro robô no caminho, com isso ele esbarra no robô e derruba o pacote
         pacote.mover(posicaoXdronefinal - posicaoXdrone, posicaoYdronefinal - posicaoYdrone); //Move o pacote junto do drone
@@ -25,12 +26,8 @@ public class Drone extends RoboAereo{
             this.descer(this.getPosicao()[2]); //Função para mover o drone para baixo
             if (this.posicaoZdrone == 0){ //Nesse ponto depois da descida o pacote foi entregue com sucesso
                 return true;
-            }
-            return false; //Caso chegue nessa parte é porque o drone encontrou um problema no caminho de descida e 
+            }  
         }
-        else {
-            return false; //Caso chegue aqui significa que o drone encontrou obstáculos no plano XY e derrubou o Rover
-        }   
-    
+        return false; //Caso chegue nessa parte é porque o drone encontrou um problema no caminho de descida ou o drone encontrou obstáculos no plano XY e derrubou o Rover
     }
 }
