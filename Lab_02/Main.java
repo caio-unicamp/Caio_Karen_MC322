1d54f6352b8850a3d16189de853f2ba9898d7f98
Karen-Rosas-174537 <k174537@unicamp.br>
2025-03-29 09:46:26 -0300

    mudancas nas strings da main

--- Lab_02/Main.java
+++ Lab_02/Main.java
@@ -110,7 +110,7 @@ public class Main {
                     System.out.println("Sinceramente eu nem sei porque os criadores desenvolveram esses daí");
                     Passaro passaro = ((Passaro) roboEscolhido);
                     //mover o pássaro
-                    System.out.println("você quer mover para onde? Passos  em x:\n");
+                    System.out.println("você quer mover para onde?\n Passos  em x:");
                     int deltaX = scanner.nextInt(); 
                     System.out.println("passos em y:");
                     int deltaY = scanner.nextInt();
