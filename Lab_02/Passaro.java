c65dd08ee7fe03d0120c9ed37c0711a7990c6920
caio lima albuquerque <c288808@dac.unicamp.br>
2025-03-29 08:58:50 -0300

    Termina a classe pássaro

--- Lab_02/Passaro.java
+++ Lab_02/Passaro.java
@@ -1,9 +1,6 @@
 public class Passaro extends RoboAereo{
     //atributo próprio da quantidade de desvios que ele fez
     private int qtdDesvios = 0;
-    private int posicaoXPassaro;
-    private int posicaoYPassaro;
-    private int posicaoZPassaro;
     private Ambiente ambiente;
     //Construtor para inicializar os atributos
     public Passaro(String nome, String direcao, int x, int y, int altitude, Ambiente ambiente){
@@ -12,65 +9,67 @@ public class Passaro extends RoboAereo{
     }
     
     public void mover(int deltaX, int deltaY) {
-        int posXinicial = this.getPosicao()[0];
-        int posYinicial = this.getPosicao()[1];
         super.mover(deltaX, deltaY);
         if (identificarRobo(this.getPosicao()[0] + this.getPasso(deltaX, deltaY)[0], this.getPosicao()[1] + this.getPasso(deltaX, deltaY)[1], this.getPosicao()[2], this.getNome())){ //Caso o pássaro identifique um obstáculo no caminho ele começa a fazer uma busca para desviar
             if (desviar(deltaX, deltaY)) {
                 qtdDesvios++;
-                this.mover(deltaX, deltaY);
-            }else {
-                System.out.println("O pássaro não conseguiu se mover!");
             }
         }
     }
 
-    //método próprio de desviar, se o Pássaro se esbarrar para alguma coisa ele desvia para: direita, esquerda, cima e baixo
-
+    //método próprio de desviar, se o Pássaro se esbarrar em alguma coisa ele tenta desviar para todas as possibilidades de direção
     private boolean desviar(int deltaX, int deltaY) {
+        String direcao = this.getDirecao();
+        int dx = 0, dy = 0;
 
-        // Testa várias direções até encontrar uma possível
-        int[][] direcoes = {
-            {1, 0, 0}, {-1, 0, 0}, {0, 1, 0}, {0, -1, 0},  
-            {1, 1, 0}, {-1, -1, 0}, {1, -1, 0}, {-1, 1, 0}, 
-            {1, 0, -1}, {-1, 0, -1}, {0, 1, -1}, {0, -1, -1},  
-            {1, 1, -1}, {-1, -1, -1}, {1, -1, -1}, {-1, 1, -1},
-            {1, 0, 1}, {-1, 0, 1}, {0, 1, 1}, {0, -1, 1},  
-            {1, 1, 1}, {-1, -1, 1}, {1, -1, 1}, {-1, 1, 1}
-        };
+        switch (direcao.toLowerCase()){ //Define o deslocamento com base na direção
+            case "norte": dy = 1;
+                break;
+            case "sul": dy = -1;
+                break;
+            case "leste": dx = 1;
+                break;
+            case "oeste": dx = -1;
+                break;
+        }  
+        // Primeiro tenta desviar no plano X-Y (3x3 à frente)
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                int novoX = this.getPosicao()[0] + dx + i;
+                int novoY = this.getPosicao()[1] + dy + j;
+                int novoZ = this.getPosicao()[2]; //Mantém a altura inicial
 
-        for (int[] dir : direcoes) {
-            int novoX = this.posicaoXPassaro + dir[0];
-            int novoY = this.posicaoYPassaro + dir[1];
-            int novoZ = this.posicaoZPassaro + dir[2];
+                if (!identificarRobo(novoX, novoY, novoZ, this.getNome()) &&
+                    ambiente.dentroDosLimites(novoX, novoY, novoZ)) {
 
-            if (!identificarRobo(novoX, novoY, novoZ, this.getNome()) && ambiente.dentroDosLimites(novoX, novoY, novoZ)){ //Se nenhum robô for encontrado na direção escolhida e ele estiver dentro dos limites, o pássaro segue nessa direção
-                super.mover(dir[0], dir[1]);
-                super.mover(deltaX - dir[0], deltaY - dir[1]);  // Tenta continuar a trajetória original
+                    // Move para a posição desviada no plano 2D
+                    super.mover(novoX - this.getPosicao()[0], novoY - this.getPosicao()[1]);
+
+                    // Tenta continuar na trajetória original
+                    super.mover(deltaX - dx, deltaY - dy);
+                    return true;
+                }
+            }
+        }
+
+        // Se não conseguiu desviar no plano X-Y, tenta desviar para cima ou para baixo (Z)
+        int[] desviosZ = {1, -1}; // Primeiro tenta subir, depois descer
+        for (int dz : desviosZ) {
+            int novoX = this.getPosicao()[0] + dx;
+            int novoY = this.getPosicao()[1] + dy;
+            int novoZ = this.getPosicao()[2] + dz;
+
+            if (!identificarRobo(novoX, novoY, novoZ, this.getNome()) &&
+                ambiente.dentroDosLimites(novoX, novoY, novoZ)) {
+
+                // Move para cima ou para baixo
+                this.getPosicao()[2] = novoZ;
+
+                // Continua a trajetória original
+                super.mover(deltaX - dx, deltaY - dy);
                 return true;
             }
         }
-        return false;
-    }
-    public boolean desviar(){
-        //se ele consegue ir para a direita, ele vai 1 posição paraa direita
-        if (mover(this.posicaoXPassaro + 1, this.posicaoYPassaro)){
-            mover(this.posicaoXPassaro + 1, this.posicaoYPassaro);
-        }
-        //se ele consegue ir para a esquerda, ele vai 1 posição paraa esquerda
-        else if (mover(this.posicaoXPassaro - 1, this.posicaoYPassaro)){
-            mover(this.posicaoXPassaro - 1, this.posicaoYPassaro);
-        }
-        //se ele consegue ir para cima, ele vai 1 posição paraa cima
-        else if (mover(this.posicaoXPassaro, this.posicaoYPassaro + 1)){
-            mover(this.posicaoXPassaro, this.posicaoYPassaro + 1);
-        }
-        //se ele consegue ir para baixo, ele vai 1 posição paraa baixo
-        else if (mover(this.posicaoXPassaro, this.posicaoYPassaro - 1)){
-            mover(this.posicaoXPassaro, this.posicaoYPassaro - 1);
-        }
-        //caso não consiga ir para nenhum dos lados, retorna falso
-        return false;
-    }
-    
+        return false; // Se não encontrou nenhuma rota alternativa
+    }            
 }
