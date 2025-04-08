540135d79f283cc1005135aa31564ec4f13fd143
caio lima albuquerque <c288808@dac.unicamp.br>
2025-04-08 15:11:28 -0300

    Cria o construtor da classe Obstáculo e adiciona um tipo de obstáculo

--- Lab_03/Obstaculo.java
+++ Lab_03/Obstaculo.java
@@ -7,15 +7,22 @@ public class Obstaculo{
     TipoObstaculo tipoObstaculo;
     Ambiente ambiente;
     
-    public Obstaculo(){
-        
+    public Obstaculo(int posX1, int posY1, int altura, int posX2, int posY2, TipoObstaculo tipoObstaculo, Ambiente ambiente){
+        this.posX1 = posX1;
+        this.posY1 = posY1;
+        this.altura = altura;
+        this.posX2 = posX2;
+        this.posY2 = posY2;
+        this.tipoObstaculo = tipoObstaculo;
+        this.ambiente = ambiente;
     }
     
     public enum TipoObstaculo{
         MINA_TERRESTRE(0, false),
+        BURACO_SEM_FUNDO(0, false),
         CAIXA(1, false),
         PASSARO(2, true),
-        ;
+        MURALHA(3, false);
 
         private final int altura;
         private final boolean obstaculoAereo;
