065a05a2bb79cc47832871f7e106fb62b8987dbb
caio lima albuquerque <c288808@dac.unicamp.br>
2025-04-08 16:49:13 -0300

    Tira os atributos de posição x, y e z do drone

--- Lab_03/Drone.java
+++ Lab_03/Drone.java
@@ -1,22 +1,19 @@
 public class Drone extends RoboAereo{
     Robo pacote; //Atributo próprio é um robô que será entregue pelo drone
-    int posicaoXdrone, posicaoYdrone, posicaoZdrone, tempoLocomocaoPacote;
+    int tempoLocomocaoPacote;
     
     public Drone(String nome, String direcao, int x, int y, int altitude, Ambiente ambiente, int tempoLocomocaoTerrestre){ //Construtor para inicializar os atributos
         super(nome, direcao, x, y, altitude, ambiente);
-        this.posicaoXdrone = x;
-        this.posicaoYdrone = y;
-        this.posicaoZdrone = altitude;
         this.tempoLocomocaoPacote = tempoLocomocaoTerrestre;
     }
 
     public boolean entregarPacote(int posicaoXdronefinal, int posicaoYdronefinal, String nomePacote, Ambiente ambiente){ //Função para entregar um pacote. Essa função é um booleano pois caso o pacote seja entregue retorna true e caso não seja, retorna false
         //inicializar o robo pacote
-        pacote = new Rover(nomePacote, this.getDirecao(), this.posicaoXdrone, this.posicaoYdrone, this.posicaoZdrone, ambiente, tempoLocomocaoPacote);
+        pacote = new Rover(nomePacote, this.getDirecao(), this.getPosicao()[0], this.getPosicao()[1], this.getPosicao()[2], ambiente, tempoLocomocaoPacote);
         ambiente.adicionarRobo(pacote); //Adiciona o pacote na lista de Robôs ativos
-        int posZinicial = this.posicaoZdrone;
+        int posZinicial = this.getPosicao()[2];
 
-        for (int i = 1; i <= Math.abs(posicaoXdronefinal - posicaoXdrone); i++){ //Faz a identificação por todo o percurso em x
+        for (int i = 1; i <= Math.abs(posicaoXdronefinal - this.getPosicao()[0]); i++){ //Faz a identificação por todo o percurso em x
             if (identificarRobo(this.getPosicao()[0] + this.getPasso(posicaoXdronefinal - this.getPosicao()[0], posicaoYdronefinal - this.getPosicao()[1])[0], this.getPosicao()[1], this.getPosicao()[2], nomePacote, ambiente)){
                 if (jaExisteRobo(pacote, ambiente)){ //Caso exista robô onde seria derrubado não precisa fazer mais nada pois ele e o pacote serão destruídos
                     return false;
@@ -27,7 +24,7 @@ public class Drone extends RoboAereo{
                 super.mover(this.getPasso(posicaoXdronefinal - this.getPosicao()[0], 0)[0], 0, ambiente);
             }
         }
-        for (int i = 1; i <= Math.abs(posicaoYdronefinal - posicaoYdrone); i++){ //Faz a identificação por todo o percurso em y
+        for (int i = 1; i <= Math.abs(posicaoYdronefinal - this.getPosicao()[1]); i++){ //Faz a identificação por todo o percurso em y
             if (identificarRobo(posicaoXdronefinal, this.getPosicao()[1] + this.getPasso(posicaoXdronefinal - this.getPosicao()[0], posicaoYdronefinal - this.getPosicao()[1])[1], this.getPosicao()[2], nomePacote, ambiente)){
                 if (jaExisteRobo(pacote, ambiente)){ //Caso exista robô onde seria derrubado não precisa fazer mais nada pois ele e o pacote serão destruídos
                     return false;
@@ -40,12 +37,12 @@ public class Drone extends RoboAereo{
         }
         
         //Atualiza a posição real do drone
-        this.posicaoXdrone = this.getPosicao()[0];
-        this.posicaoYdrone = this.getPosicao()[1];
+        this.getPosicao()[0] = this.getPosicao()[0];
+        this.getPosicao()[1] = this.getPosicao()[1];
         
         pacote.setPosicao(posicaoXdronefinal, posicaoYdronefinal, 0); //Como o pacote não é um robô aéreo e não tem um método de descer, seta a coordenada z dele no chão independentemente se ele foi entregue ou foi derrubado
 
-        if ((this.posicaoXdrone == posicaoXdronefinal) && (this.posicaoYdrone == posicaoYdronefinal)){ //se chegar na posição que ele quer, ele desce com o robo            
+        if ((this.getPosicao()[0] == posicaoXdronefinal) && (this.getPosicao()[1] == posicaoYdronefinal)){ //se chegar na posição que ele quer, ele desce com o robo            
             this.descer(this.getPosicao()[2] - 1, ambiente); //Desce o drone até uma unidade antes do chão para conseguir entregar o pacote
             if (this.getPosicao()[2] == 1){ //Nesse ponto depois da descida o pacote foi entregue com sucesso
                 this.setPosicao(this.getPosicao()[0], this.getPosicao()[1], posZinicial); //Após a entrega o drone sobe para a altura que estava no início, como a função descer já analisou se existiam obstáculos, basta apenas setar a posição z como a mesma do início sem necessidade de nenhuma conferência extra
