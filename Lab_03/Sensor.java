068bbb9ed6752e194ada02d914273f646b021b75
caio lima albuquerque <c288808@dac.unicamp.br>
2025-04-10 13:32:21 -0300

    Adiciona atributo de bateria aos sensores e métodos para trabalhar com essa bateria

--- Lab_03/Sensor.java
+++ Lab_03/Sensor.java
@@ -1,9 +1,39 @@
 abstract class Sensor<T> {
     private double raio;
+    private int bateria;
     
     Sensor(double raio){
         this.raio = raio;
+        this.bateria = 100; // Bateria inicial
     }
     
     abstract T monitorar();
+
+    public int getBateria() {
+        return bateria;
+    }
+    public void setBateria(int bateria) {
+        this.bateria = bateria;
+    }
+    public double getRaio() {
+        return raio;
+    }
+    public void setRaio(double raio) {
+        this.raio = raio;
+    }
+    public void recarregarBateria(int carga) {
+        this.bateria += carga;
+        if (this.bateria > 100) {
+            this.bateria = 100; // Bateria não pode exceder 100%
+        }
+    }
+    public void consumirBateria(int consumo) {
+        this.bateria -= consumo;
+        if (this.bateria < 0) {
+            this.bateria = 0; // Bateria não pode ser negativa
+        }
+    }
+    public boolean isBateriaBaixa() {
+        return this.bateria < 20; // Considera bateria baixa se estiver abaixo de 20%
+    }
 }
