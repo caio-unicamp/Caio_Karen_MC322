c64ea6a1599f3f4860ffa2793ff81c75782f6850
caio lima albuquerque <c288808@dac.unicamp.br>
2025-04-28 20:26:32 -0300

    Finaliza o mover e o entregar pacote do drone, retira a função remover pacote pois agora se algo de errado acontece no trajeto ele simplesmente não irá adicionar o pacote

--- Lab_03/Drone.java
+++ Lab_03/Drone.java
@@ -50,6 +50,14 @@ public class Drone extends RoboAereo{
             this.descer(this.getPosicao()[2] - 1, ambiente); //Desce o drone até uma unidade antes do chão para conseguir entregar o pacote
             if (this.getPosicao()[2] == 1){ //Nesse ponto depois da descida o pacote foi entregue com sucesso
                 this.setPosicao(this.getPosicao()[0], this.getPosicao()[1], posInicialZ); //Após a entrega o drone sobe para a altura que estava no início, como a função descer já analisou se existiam obstáculos, basta apenas setar a posição z como a mesma do início sem necessidade de nenhuma conferência extra
+                for (Obstaculo obstaculo: ambiente.getListaObstaculos()){
+                    if (obstaculo.getPosX1() <= this.getPosicao()[0] && obstaculo.getPosX2() >= this.getPosicao()[0] && obstaculo.getPosY1() <= this.getPosicao()[1] && obstaculo.getPosY2() >= this.getPosicao()[1] && obstaculo.getAltura() >= this.getPosicao()[2]){ //Se já existe um obstáculo no lugar que o pacote seria derrubado, ele será destruído
+                        if (obstaculo.getTipoObstaculo().equals(TipoObstaculo.MINA_TERRESTRE)){ // No caso do pacote cair em uma mina, ela é destruída
+                            ambiente.removerObstaculo(obstaculo);
+                        }
+                        return false;
+                    }
+                }
                 if (jaExisteRobo(ambiente)){ //Caso exista robô onde seria entregue, ele e o pacote serão destruídos
                     for (Robo robo : ambiente.getListaRobos()){
                         if (robo.getPosicao()[0] == this.getPosicao()[0] && robo.getPosicao()[1] == this.getPosicao()[1] && robo.getPosicao()[2] == 0){ //Se já existe um robô no lugar que o pacote seria derrubado, ele será destruído
@@ -69,44 +77,7 @@ public class Drone extends RoboAereo{
                 }
             }
         }
-
-        for (int i = 1; i <= Math.abs(posicaoXdronefinal - this.getPosicao()[0]); i++){ //Faz a identificação por todo o percurso em x
-            if (identificarRobo(this.getPosicao()[0] + this.getPasso(posicaoXdronefinal - this.getPosicao()[0], posicaoYdronefinal - this.getPosicao()[1])[0], this.getPosicao()[1], this.getPosicao()[2], ambiente)){
-                if (jaExisteRobo(pacote, ambiente)){ //Caso exista robô onde seria derrubado não precisa fazer mais nada pois ele e o pacote serão destruídos
-                    return false;
-                }
-                pacote.setPosicao(this.getPosicao()[0], this.getPosicao()[1], 0); //Derruba o pacote
-                return false; //Se identificou um obstáculo a entrega falhou
-            }else{
-                super.mover(this.getPasso(posicaoXdronefinal - this.getPosicao()[0], 0)[0], 0, ambiente);
-            }
-        }
-        for (int i = 1; i <= Math.abs(posicaoYdronefinal - this.getPosicao()[1]); i++){ //Faz a identificação por todo o percurso em y
-            if (identificarRobo(posicaoXdronefinal, this.getPosicao()[1] + this.getPasso(posicaoXdronefinal - this.getPosicao()[0], posicaoYdronefinal - this.getPosicao()[1])[1], this.getPosicao()[2], ambiente)){
-                if (jaExisteRobo(pacote, ambiente)){ //Caso exista robô onde seria derrubado não precisa fazer mais nada pois ele e o pacote serão destruídos
-                    return false;
-                }
-                pacote.setPosicao(this.getPosicao()[0], this.getPosicao()[1], 0); //Derruba o pacote
-                return false; //Se identificou um obstáculo a entrega falhou
-            }else{
-                super.mover(0, this.getPasso(0 , posicaoYdronefinal - this.getPosicao()[1])[1], ambiente);
-            }
-        }
-        
-        //Atualiza a posição real do drone
-        this.getPosicao()[0] = this.getPosicao()[0];
-        this.getPosicao()[1] = this.getPosicao()[1];
-        
-        pacote.setPosicao(posicaoXdronefinal, posicaoYdronefinal, 0); //Como o pacote não é um robô aéreo e não tem um método de descer, seta a coordenada z dele no chão independentemente se ele foi entregue ou foi derrubado
-
-        if ((this.getPosicao()[0] == posicaoXdronefinal) && (this.getPosicao()[1] == posicaoYdronefinal)){ //se chegar na posição que ele quer, ele desce com o robo            
-            this.descer(this.getPosicao()[2] - 1, ambiente); //Desce o drone até uma unidade antes do chão para conseguir entregar o pacote
-            if (this.getPosicao()[2] == 1){ //Nesse ponto depois da descida o pacote foi entregue com sucesso
-                this.setPosicao(this.getPosicao()[0], this.getPosicao()[1], posInicialZ); //Após a entrega o drone sobe para a altura que estava no início, como a função descer já analisou se existiam obstáculos, basta apenas setar a posição z como a mesma do início sem necessidade de nenhuma conferência extra
-                return true;
-            }  
-        }
-        return false; //Caso chegue nessa parte é porque o drone encontrou um problema no caminho de descida ou o drone encontrou obstáculos no plano XY e derrubou o Rover
+        return false; // Caso o robô não tenha conseguido entregar o pacote, retorna false
     }
 
     public boolean jaExisteRobo(Ambiente ambiente){
@@ -121,10 +92,6 @@ public class Drone extends RoboAereo{
     public void adicionaPacote(String nomePacote, Ambiente ambiente){ //Função para adicionar o pacote na lista de robôs ativos
         pacote = new Rover(nomePacote, this.getDirecao(), this.getPosicao()[0], this.getPosicao()[1], this.getPosicao()[2], tempoLocomocaoPacote);
         ambiente.adicionarRobo(pacote); //Adiciona o pacote na lista de Robôs ativos
-    }
-    
-    public void destroiPacote(Robo pacote, Ambiente ambiente){
-        ambiente.removerRobo(pacote);
     }
 
     public void destroiRoboColidido(Robo roboColidido, Ambiente ambiente){ //Função para destruir o robô colidido
