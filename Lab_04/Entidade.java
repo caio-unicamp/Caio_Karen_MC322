e08361d5f5a363a512beaac7324c4fd61533dc13
caio lima albuquerque <c288808@dac.unicamp.br>
2025-05-24 17:26:09 -0300

    Transforma a classe Entidade em uma interface

--- Lab_04/Entidade.java
+++ Lab_04/Entidade.java
@@ -1,45 +1,21 @@
-public class Entidade {
-    private final int x;
-    private final int y;
-    private final int z;
-    private final TipoEntidade tipoEntidade; //Tipo da entidade de acordo com o enum referente a ele
+// Entidade.java
 
-    public Entidade(TipoEntidade tipoEntidade, int x, int y, int z){ //Constructor da Entidade
-        this.tipoEntidade = tipoEntidade;
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
+/**
+ * Interface que define o contrato para todas as entidades que podem existir no ambiente.
+ * Garante que qualquer objeto no ambiente tenha uma posição, tipo, descrição e representação.
+ */
 
-    public TipoEntidade getTipoEntidade(){ //Retorna o tipo da entidade de acordo com o enum de entidades 
-        return tipoEntidade;
-    }
+public interface Entidade {
+    public int getX(); //Retorna a posição x da entidade
 
-    public String getDescricao(){ //Retorna uma descrição referente à entidade em questão
-        if (this.getTipoEntidade().equals(TipoEntidade.VAZIO)){ //Descrição do VAZIO
-            return "VAZIO";
-        }else if (this.getTipoEntidade().equals(TipoEntidade.ROBO)){ //Descrição do ROBO
-            return "ROBO";
-        }else if (this.getTipoEntidade().equals(TipoEntidade.OBSTACULO)){ //Descrição do OBSTACULO
-            return "OBSTACULO";
-        }else{ //Descrição do DESCONHECIDO
-            return "Entidade desconhecida, tem certeza que você escreveu corretamente?";
-        }
-    }
+    public int getY(); //Retorna a posição y da entidade
 
-    public char getSimbolo(){ //Retorna o símbolo de representação da entidade
-        return this.getTipoEntidade().getSimbolo();
-    }
+    public int getZ(); //Retorna a posição z da entidade
 
-    public int getX(){ //Retorna a posição x da entidade
-        return this.x;
-    }
+    TipoEntidade getTipoEntidade(); //Retorna o tipo da entidade de acordo com o enum de entidades 
 
-    public int getY(){ //Retorna a posição y da entidade
-        return this.y;
-    }
+    public String getDescricao(); //Retorna uma descrição referente à entidade em questão
 
-    public int getZ(){ //Retorna a posição z da entidade
-        return this.z;
-    }
+    public char getSimbolo(); //Retorna o símbolo de representação da entidade
+
 }
