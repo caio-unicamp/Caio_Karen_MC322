fb33dcaeba864f3d4be57cae6158429cd6fd5440
caio lima albuquerque <c288808@dac.unicamp.br>
2025-05-24 18:23:01 -0300

    Altera o jeito que o mapa é criado passando de hash para um tensor, deixando de ser necessário a classe Posicao

--- Lab_04/Ambiente.java
+++ Lab_04/Ambiente.java
@@ -1,6 +1,4 @@
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Ambiente{
     private final String ambiente; //nome do ambiente
@@ -8,14 +6,14 @@ public class Ambiente{
     private final int tamY; //tamanho em Y do ambiente
     private final int tamZ; //Tamanho em Z do ambiente no caso do robô aéreo
     private ArrayList<Entidade> listaEntidades; //Lista das entidades ativas presentes no ambiente 
-    private Entidade[][][] mapa;
+    private TipoEntidade[][][] mapa;
     
     public Ambiente(String ambiente, int tamX, int tamY, int tamZ){ //Construtor para inicializar os atributos
         this.ambiente = ambiente;
         this.tamX = tamX;
         this.tamY = tamY;
         this.tamZ = tamZ;
-        this.mapa = new Entidade[this.tamX - 1][this.tamY - 1][this.tamZ - 1];
+        this.mapa = new TipoEntidade[this.tamX - 1][this.tamY - 1][this.tamZ - 1];
         listaEntidades = new ArrayList<>();
     }
     public String getNomeAmbiente(){ //retorna o nome do ambiente 
@@ -31,43 +29,14 @@ public class Ambiente{
         return (x >= 0 && x <= this.tamX && y >= 0 && y <= this.tamY && altura >= 0 && altura <= this.tamZ);
     }
 
-    public void adicionarRobo(Robo robo){ //Adiciona um robô na lista de robôs ativos 
-        listaRobosAtivos.add(robo);
-    }
-
-    public void removerRobo(Robo robo){ //Remove um robo na lista de robos ativos
-        listaRobosAtivos.remove(robo);
-    }
-
-    public ArrayList<Robo> getListaRobos(){ //Retorna a lista de Robôs ativos no ambiente
-        return listaRobosAtivos;
-    }
-
-    public void adicionarObstaculo(Obstaculo obstaculo){ //Adiciona um obstáculo no ambiente
-        listaObstaculos.add(obstaculo);
-    }
-
-    public void removerObstaculo(Obstaculo obstaculo){ //Remove um obstáculo no ambiente
-        listaObstaculos.remove(obstaculo);
-    }
-    
-    public ArrayList<Obstaculo> getListaObstaculos(){ //Retorna a lista de obstáculos ativos no ambiente
-        return listaObstaculos;
-    }
-
-    public void adicionarEntidade(int x, int y, int z, Entidade entidade){ //Adiciona a entidade no dicionário com sua respectiva chave 
-        Posicao pos = new Posicao(x, y, z); //Cria a chave de acordo com as coordenadas
-        mapa.putIfAbsent(pos, entidade); //Só adiciona a entidade nessa chave de coordenada se ela não está relacionada a nenhum valor
-    }
-
-    public void removerEntidade(Entidade entidade){
-        Posicao pos = new Posicao(entidade.getX(), entidade.getY(), entidade.getZ()); //Procura a chave de acordo com o método get das coordenadas da entidade
-        mapa.remove(pos);
-    }
-    
-    public Entidade getEntidade(int x, int y, int z){
-        Posicao pos = new Posicao(x, y, z);
-        return mapa.get(pos); //Retorna NULL se a chave não existir no mapa
+    private void inicializarMapa(){
+        for (int x = 0; x < this.tamX; x++){
+            for (int y = 0; y < this.tamY; y++){
+                for (int z  = 0; z < this.tamZ; z++){
+                    mapa[x][y][z] = TipoEntidade.VAZIO;
+                }
+            }
+        }
     }
     
 }
