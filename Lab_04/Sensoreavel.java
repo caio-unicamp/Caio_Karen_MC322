343df0d9239dd6c5f0b00a56c3519dd6599ba874
caio lima albuquerque <c288808@dac.unicamp.br>
2025-05-27 16:23:51 -0300

    Comentários na interface sensoreavel

--- Lab_04/Sensoreavel.java
+++ Lab_04/Sensoreavel.java
@@ -5,7 +5,7 @@ public interface Sensoreavel {
     /**
      * Aciona os sensores do robô.
      * @throws RoboDesligadoException se o robô tentar usar os sensores enquanto estiver desligado.
-     * @throws SensorDesligadoException se os sensores estiverem descarregados.
+     * @throws SensorDesligadoException se algum sensore estiver descarregado.
      */
     void acionarSensores() throws RoboDesligadoException, SensorDesligadoException; 
 }
