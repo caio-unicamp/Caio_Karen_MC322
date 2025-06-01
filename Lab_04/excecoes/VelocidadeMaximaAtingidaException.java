47d5a36e15a3f4915ce41631ca49f70baa39a359
caio lima albuquerque <c288808@dac.unicamp.br>
2025-06-01 15:02:56 -0300

    Cria exceção de altura máxima atingida

--- Lab_04/excecoes/VelocidadeMaximaAtingidaException.java
+++ Lab_04/excecoes/VelocidadeMaximaAtingidaException.java
@@ -3,7 +3,10 @@ package excecoes;
  * Exceção lançada quando um robô terrestre tenta exceder o limite de velocidade permitido para ele.
  */
 public class VelocidadeMaximaAtingidaException extends Exception{
-
+    /**
+     * Passe a mensagem completa indicando qual o erro
+     * @param message
+     */
     public VelocidadeMaximaAtingidaException(String message){
         super(message);
     }
