a6bb4f27e3f43b3c3abee8ced5e7ebdcef36a43b
caio lima albuquerque <c288808@dac.unicamp.br>
2025-06-10 15:44:52 -0300

    Remove diagrama antigo

--- Lab_05/robots/AgenteInteligente.java
+++ Lab_05/robots/AgenteInteligente.java
@@ -4,12 +4,36 @@ import missions.*;
 
 public abstract class AgenteInteligente extends Robo{
     protected Missao missao;
-    
+    /**
+     * Construtor da classe AgenteInteligente.
+     * Este construtor inicializa o agente inteligente com um ID, direção e coordenadas no ambiente.
+     * @param idRobo representando seu nome
+     * @param direcaoRobo 
+     * @param x
+     * @param y
+     * @param z
+     */
     public AgenteInteligente(String idRobo, String direcaoRobo, int x, int y, int z) {
         super(idRobo, direcaoRobo, x, y, z);
     }
-
+    /**
+     * Executa a missão do agente inteligente no ambiente.
+     * @param ambiente 
+     */
+    public abstract void executarMissao(Ambiente ambiente);
+    /**
+     * Define a missão do agente inteligente.
+     * @param missao
+     */
     public void definirMissao(Missao missao){
         this.missao = missao;
     }
+    /**
+     * Verifica se o agente inteligente tem uma missão definida.
+     * @return true se tem missão, false caso contrário.
+     */
+    public boolean temMissao() {
+        return this.missao != null;
+    }
+
 }
