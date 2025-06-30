043464609d3d7cbbd0abdabb4f878d838439bbee
caio lima albuquerque <c288808@dac.unicamp.br>
2025-06-29 21:58:20 -0300

    Agora o aspirador nao se move quando desligado

--- Lab_05/Main.java
+++ Lab_05/Main.java
@@ -149,37 +149,34 @@ public class Main {
                     Aspirador aspirador = ((Aspirador) roboEscolhido);
                     if (comando == 1){ //Método de mover normal do Aspirador
                         System.out.println("Quanto você deseja mover ele? Lembre-se que destruirá todos os robôs no caminho, faça o que quiser com essa informação...");
-                        while (true){
-                            int deltaX = lerInteiro("Passos em x: ", scanner); 
-                            int deltaY = lerInteiro("Passos em y: ", scanner);
-                            try{
-                                double taxaAspirador = aspirador.getSensorVelocidade().porcentoVelocidade(aspirador.getSensorVelocidade().monitorar(deltaX, deltaY, aspirador), aspirador.getVelocidadeMaxima());
-                                String velAspirador = String.format("%.2f", taxaAspirador);
-                                
-                                aspirador.mover(deltaX, deltaY, ambiente);
+                        int deltaX = lerInteiro("Passos em x: ", scanner); 
+                        int deltaY = lerInteiro("Passos em y: ", scanner);
+                        try{
+                            double taxaAspirador = aspirador.getSensorVelocidade().porcentoVelocidade(aspirador.getSensorVelocidade().monitorar(deltaX, deltaY, aspirador), aspirador.getVelocidadeMaxima());
+                            String velAspirador = String.format("%.2f", taxaAspirador);
+                            
+                            aspirador.mover(deltaX, deltaY, ambiente);
 
-                                obstaculoAchado(aspirador, ambiente);
-                                System.out.println("Você andou a "+ velAspirador + "% da velocidade máxima");
-                                if (aspirador.getSensorVelocidade().isMuito(taxaAspirador)){
-                                    System.out.println("Quase um SpeedRacer! Impressionante!");
-                                }
-                                //pegar a qtd de robos eliminados
-                                int qtdEliminados = aspirador.getRobosEliminados();
-                                //imprimir a qtd de eliminados
-                                System.out.println("Até agora você destruiu " + qtdEliminados + " robôs na sua vida");
-                                if (qtdEliminados > 10){
-                                    System.out.println("Um tremendo massacre eu diria, chega a me assustar, o próximo pode ser eu");
-                                }
-                                break;    
-                            }catch(RoboDesligadoException e){ //O robô só pode se mover se estiver ligado
-                                System.err.println(e.getMessage());
-                            }catch(SensorDesligadoException e){ //Se ele tentar se mover com um sensor descarregado ele indica que isso não é possível
-                                System.err.println(e.getMessage());
-                            }catch(ColisaoException e){ //Se ele se moveu e acabou a bateria do sensor de proximidade no meio da locomoção ele poderá colidir com algum obstáculo
-                                System.err.println(e.getMessage());
-                            }catch(VelocidadeMaximaAtingidaException e){ //Se ele tiver ultrapassado o limite de velocidade ele irá pedir novamente o quanto ele quer que o robô ande e indicará qual foi o erro
-                                System.err.println(e.getMessage());
+                            obstaculoAchado(aspirador, ambiente);
+                            System.out.println("Você andou a "+ velAspirador + "% da velocidade máxima");
+                            if (aspirador.getSensorVelocidade().isMuito(taxaAspirador)){
+                                System.out.println("Quase um SpeedRacer! Impressionante!");
                             }
+                            //pegar a qtd de robos eliminados
+                            int qtdEliminados = aspirador.getRobosEliminados();
+                            //imprimir a qtd de eliminados
+                            System.out.println("Até agora você destruiu " + qtdEliminados + " robôs na sua vida");
+                            if (qtdEliminados > 10){
+                                System.out.println("Um tremendo massacre eu diria, chega a me assustar, o próximo pode ser eu");
+                            }
+                        }catch(RoboDesligadoException e){ //O robô só pode se mover se estiver ligado
+                            System.err.println(e.getMessage());
+                        }catch(SensorDesligadoException e){ //Se ele tentar se mover com um sensor descarregado ele indica que isso não é possível
+                            System.err.println(e.getMessage());
+                        }catch(ColisaoException e){ //Se ele se moveu e acabou a bateria do sensor de proximidade no meio da locomoção ele poderá colidir com algum obstáculo
+                            System.err.println(e.getMessage());
+                        }catch(VelocidadeMaximaAtingidaException e){ //Se ele tiver ultrapassado o limite de velocidade ele irá pedir novamente o quanto ele quer que o robô ande e indicará qual foi o erro
+                            System.err.println(e.getMessage());
                         }
                     }else if (comando == 2){ //Para ir atás de aspirar um robô específico
                         System.out.println("EXCELSIOR! Nesse modo o aspirador fica insano e vai caçar um robô em específico. Qual deles vai ser o azarado da vez? Digite o nome do robô a ser aspirado");
